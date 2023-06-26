b8fabe9dc37878b34e4bfb6410aa292ad5732f9b
xiaohai-huang <jeromehuang18@gmail.com>
2023-06-26 07:52:11 -0600

    Make GameManager into singleton

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,6 +6,19 @@ using Xiaohai.Input;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private InputReader _inputReader;
+    public GameObject Player;
+    public static GameManager Instance { get; private set; }
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
