3197699be671459a7c7b152cf620564e8683490f
xiaohai-huang <jeromehuang18@gmail.com>
2023-06-27 03:15:36 -0600

    feat: add 4 modes for Face Camera

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    public enum Mode
+    {
+        LookAtCamera,
+        LookAtCameraInverted,
+        CameraForward,
+        CameraForwardInverted
+    }
+    public Mode LookAtMode;
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        switch (LookAtMode)
+        {
+            case Mode.LookAtCamera:
+                transform.LookAt(Camera.main.transform);
+                break;
+            case Mode.LookAtCameraInverted:
+                transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+                break;
+            case Mode.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+            case Mode.CameraForwardInverted:
+                transform.forward = -Camera.main.transform.forward;
+                break;
+        }
+    }
+}
