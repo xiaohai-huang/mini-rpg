ca9558075aacfb95d0890952116bfa701896858d
xiaohai-huang <jeromehuang18@gmail.com>
2023-07-07 10:30:31 +0800

    Add Range Indicator

--- Assets/Scripts/UI/Characters/AbilityButton/AbilityDirectionIndicator.cs
+++ Assets/Scripts/UI/Characters/AbilityButton/AbilityDirectionIndicator.cs
@@ -3,36 +3,44 @@ using UnityEngine;
 public class AbilityDirectionIndicator : MonoBehaviour
 {
     [SerializeField] private AbilityButton _button;
-    [SerializeField] private Transform _indicator;
-    [SerializeField] private Transform _body;
-    [SerializeField] private Transform _head;
+    [SerializeField] private Transform _arrowIndicator;
+    [SerializeField] private Transform _rangeIndicator;
+    [SerializeField] private Transform _arrowBody;
+    [SerializeField] private Transform _arrowHead;
+    [SerializeField] private Transform _circle;
     [SerializeField] private Color _activeColor;
     [SerializeField] private Color _cancelColor;
-    private Material _bodyMaterial;
-    private Material _headMaterial;
+    private Material _arrowBodyMaterial;
+    private Material _arrowHeadMaterial;
+    private Material _rangeIndicatorMaterial;
 
     [SerializeField] private Transform _target;
     [SerializeField] private float _length;
 
     void Awake()
     {
-        _indicator.gameObject.SetActive(false);
+        _arrowIndicator.gameObject.SetActive(false);
 
         // update the length of the body and position of the head
-        var newScale = _body.transform.localScale;
+        var newScale = _arrowBody.transform.localScale;
         newScale.y = _length;
-        _body.transform.localScale = newScale;
+        _arrowBody.transform.localScale = newScale;
 
-        var newPosition = _body.transform.localPosition;
+        var newPosition = _arrowBody.transform.localPosition;
         newPosition.z = _length / 2;
-        _body.transform.localPosition = newPosition;
+        _arrowBody.transform.localPosition = newPosition;
 
-        var newHeadPosition = _head.transform.localPosition;
+        var newHeadPosition = _arrowHead.transform.localPosition;
         newHeadPosition.z = _length;
-        _head.transform.localPosition = newHeadPosition;
+        _arrowHead.transform.localPosition = newHeadPosition;
 
-        _bodyMaterial = _body.GetComponent<MeshRenderer>().material;
-        _headMaterial = _head.GetComponent<MeshRenderer>().material;
+        // update the size of the range indicator
+        _circle.transform.localScale = new Vector3(_length * 2, _length * 2, 1);
+
+
+        _arrowBodyMaterial = _arrowBody.GetComponent<MeshRenderer>().material;
+        _arrowHeadMaterial = _arrowHead.GetComponent<MeshRenderer>().material;
+        _rangeIndicatorMaterial = _circle.GetComponent<MeshRenderer>().material;
     }
 
     void OnEnable()
@@ -40,6 +48,7 @@ public class AbilityDirectionIndicator : MonoBehaviour
         _button.OnBeginInteraction += OnBeginInteraction;
         _button.OnMoving += OnMoving;
         _button.OnReleased += OnReleased;
+        _button.OnCancellingChanged += OnCancellingChanged;
     }
 
     void OnDisable()
@@ -47,35 +56,42 @@ public class AbilityDirectionIndicator : MonoBehaviour
         _button.OnBeginInteraction -= OnBeginInteraction;
         _button.OnMoving -= OnMoving;
         _button.OnReleased -= OnReleased;
+        _button.OnCancellingChanged -= OnCancellingChanged;
     }
 
     private void OnMoving()
     {
         var dir = new Vector3(_button.Direction.x, 0, _button.Direction.y);
-        _indicator.rotation = Quaternion.LookRotation(dir, Vector3.up);
-        // turn red if the player is trying to cancel the action
-        if (_button.Cancelling)
-        {
-            _bodyMaterial.color = _cancelColor;
-            _headMaterial.color = _cancelColor;
-        }
-        else
-        {
-            _bodyMaterial.color = _activeColor;
-            _headMaterial.color = _activeColor;
-        }
+        _arrowIndicator.rotation = Quaternion.LookRotation(dir, Vector3.up);
     }
 
     private void OnReleased(bool arg0)
     {
-        _indicator.gameObject.SetActive(false);
-        _bodyMaterial.color = _activeColor;
-        _headMaterial.color = _activeColor;
+        _arrowIndicator.gameObject.SetActive(false);
+        _rangeIndicator.gameObject.SetActive(false);
     }
 
     private void OnBeginInteraction()
     {
-        _indicator.rotation = _target.transform.rotation;
-        _indicator.gameObject.SetActive(true);
+        _arrowIndicator.rotation = _target.transform.rotation;
+        _arrowIndicator.gameObject.SetActive(true);
+        _rangeIndicator.gameObject.SetActive(true);
+    }
+
+    private void OnCancellingChanged(bool cancelling)
+    {
+        // turn red if the player is trying to cancel the action
+        if (cancelling)
+        {
+            _arrowBodyMaterial.color = _cancelColor;
+            _arrowHeadMaterial.color = _cancelColor;
+            _rangeIndicatorMaterial.SetColor("_Color", _cancelColor);
+        }
+        else
+        {
+            _arrowBodyMaterial.color = _activeColor;
+            _arrowHeadMaterial.color = _activeColor;
+            _rangeIndicatorMaterial.SetColor("_Color", _activeColor);
+        }
     }
 }
