5fcc6edc87120dcc7725b6ca0e7499e1aad12707
xiaohai-huang <jeromehuang18@gmail.com>
2023-07-30 04:58:25 -0600

    fix: basic attack enum bug which causes  ability index to shift by 1

--- Assets/Scripts/Character/Marco Polo/Actions/BasicAttackActionSO.cs
+++ Assets/Scripts/Character/Marco Polo/Actions/BasicAttackActionSO.cs
@@ -4,20 +4,22 @@ using UOP1.StateMachine.ScriptableObjects;
 
 namespace Xiaohai.Character.MarcoPolo
 {
-    [CreateAssetMenu(fileName = "FireBulletAction", menuName = "State Machines/Actions/Marco Polo/Fire Bullet Action")]
-    public class FireBulletActionSO : StateActionSO
+    [CreateAssetMenu(fileName = "Basic Attack Action", menuName = "State Machines/Actions/Marco Polo/Basic Attack Action")]
+    public class BasicAttackActionSO : StateActionSO
     {
         protected override StateAction CreateAction() => new FireBulletAction();
     }
 
     public class FireBulletAction : StateAction
     {
-        private MarcoPolo _marcoPolo;
-        protected new FireBulletActionSO OriginSO => (FireBulletActionSO)base.OriginSO;
+        protected new BasicAttackActionSO OriginSO => (BasicAttackActionSO)base.OriginSO;
+        private Character _character;
+        private MarcoPolo _polo;
 
         public override void Awake(StateMachine stateMachine)
         {
-            _marcoPolo = stateMachine.GetComponent<MarcoPolo>();
+            _character = stateMachine.GetComponent<Character>();
+            _polo = stateMachine.GetComponent<MarcoPolo>();
         }
 
         public override void OnUpdate()
@@ -26,7 +28,8 @@ namespace Xiaohai.Character.MarcoPolo
 
         public override void OnStateEnter()
         {
-            _marcoPolo.Attack();
+            _polo.Attack();
+            _character.BasicAttackInput = false;
         }
 
         public override void OnStateExit()
