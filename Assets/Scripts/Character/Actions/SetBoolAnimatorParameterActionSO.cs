85dcb696ec642880440ec153bc579109b126df5a
xiaohai-huang <jeromehuang18@gmail.com>
2023-08-14 19:45:32 -0600

    feat: add SetBoolAnimatorParameterAction

---
+++ Assets/Scripts/Character/Actions/SetBoolAnimatorParameterActionSO.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UOP1.StateMachine;
+using UOP1.StateMachine.ScriptableObjects;
+
+[CreateAssetMenu(fileName = "SetBoolAnimatorParameterActionSO", menuName = "State Machines/Actions/Set Bool Animator Param Action")]
+public class SetBoolAnimatorParameterActionSO : StateActionSO
+{
+	public string Parameter;
+	public bool Value;
+	protected override StateAction CreateAction() => new SetBoolAnimatorParamAction();
+}
+
+public class SetBoolAnimatorParamAction : StateAction
+{
+	protected new SetBoolAnimatorParameterActionSO OriginSO => (SetBoolAnimatorParameterActionSO)base.OriginSO;
+
+	private Animator _animator;
+	private static int ANIMATION_ID;
+	public override void Awake(StateMachine stateMachine)
+	{
+		_animator = stateMachine.GetComponent<Animator>();
+		ANIMATION_ID = Animator.StringToHash(OriginSO.Parameter);
+	}
+
+	public override void OnUpdate()
+	{
+	}
+
+	public override void OnStateEnter()
+	{
+		_animator.SetBool(ANIMATION_ID, OriginSO.Value);
+	}
+
+	public override void OnStateExit()
+	{
+	}
+}
