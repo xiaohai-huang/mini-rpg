dd250be27a74ff4c5f0ddde3ee483ab50cb19dab
xiaohai-huang <jeromehuang18@gmail.com>
2023-09-13 10:24:42 +0800

    feat(Arthur): show enhanced basic indicator

--- Assets/Scripts/Character/Arthur/Effects/AbilityOneEffectSO.cs
+++ Assets/Scripts/Character/Arthur/Effects/AbilityOneEffectSO.cs
@@ -5,6 +5,8 @@ namespace Xiaohai.Character.Arthur
     [CreateAssetMenu(fileName = "Arthur Ability One Speed Up Effect", menuName = "My Scriptable Objects/Effects/Character/Arthur/ Ability One Speed Up Effect")]
     public class AbilityOneEffectSO : EffectSO<AbilityOneEffect>
     {
+        public GameObject RangeIndicatorPrefab;
+        public Vector3 Offset;
     }
 
     /// <summary>
@@ -12,10 +14,12 @@ namespace Xiaohai.Character.Arthur
     /// </summary>
     public class AbilityOneEffect : Effect
     {
+        public new AbilityOneEffectSO OriginSO => (AbilityOneEffectSO)base.OriginSO;
         private float _speedPercentage;
         private float _enhancedBasicAttackRange;
         private Character _character;
         private int _timerId;
+        private GameObject _indicator;
 
         /// <summary>
         /// 
@@ -35,13 +39,19 @@ namespace Xiaohai.Character.Arthur
             _character = system.GetComponent<Character>();
             _character.BonusWalkSpeed += _speedPercentage * _character.BaseWalkSpeed;
 
-            // TODO: show the visual of enhanced basic attack range indicator
+            // show the visual of enhanced basic attack range indicator
+            _indicator = Object.Instantiate(OriginSO.RangeIndicatorPrefab, system.transform);
+            _indicator.transform.localScale = new Vector3(_enhancedBasicAttackRange * 2, _enhancedBasicAttackRange * 2, 1f);
+            _indicator.transform.localPosition = Vector3.zero;
+            _indicator.transform.localPosition += OriginSO.Offset;
+            _indicator.SetActive(true);
             _timerId = Timer.Instance.SetTimeout(() => Finished = true, CoolDownTime);
         }
 
         public override void OnRemove(EffectSystem system)
         {
             base.OnRemove(system);
+            Object.Destroy(_indicator);
             _character.BonusWalkSpeed -= _speedPercentage * _character.BaseWalkSpeed;
             Timer.Instance.ClearTimeout(_timerId);
         }
