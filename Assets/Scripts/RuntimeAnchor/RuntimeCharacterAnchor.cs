96df9ca92017784ec485c201b1202b06de071362
xiaohai-huang <jeromehuang18@gmail.com>
2023-09-28 16:11:42 +0800

    feat(UI): Add target indicator (#7)

---
+++ Assets/Scripts/RuntimeAnchor/RuntimeCharacterAnchor.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+using Xiaohai.Character;
+
+[CreateAssetMenu(fileName = "Character Anchor", menuName = "My Scriptable Objects/Runtime Anchor/Character")]
+public class RuntimeCharacterAnchor : RuntimeAnchorBase<Character> { }
---
+++ Assets/Scripts/UI/Characters/Target Indicator/TargetIndicator.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Xiaohai.Character;
+
+namespace Xiaohai.UI
+{
+    public class TargetIndicator : MonoBehaviour
+    {
+        [SerializeField]
+        private RuntimeCharacterAnchor _player;
+        [SerializeField]
+        private GameObject _circle;
+        [SerializeField]
+        private Damageable _damageable;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_player.Value.Target == _damageable)
+            {
+                _circle.SetActive(true);
+            }
+            else
+            {
+                _circle.SetActive(false);
+            }
+        }
+    }
+}
