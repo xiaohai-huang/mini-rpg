c57f612c97ec9e7ace94a1d8bd3092a49bac0b74
xiaohai-huang <jeromehuang18@gmail.com>
2024-05-13 14:20:48 +0800

    feat(XiaoQiao): Add basic movement

---
+++ Assets/Core/Heros/XiaoQiao/XiaoQiao.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Xiaohai.Character.XiaoQiao
+{
+    public class XiaoQiao : Character
+    {
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+
+    }
+}
